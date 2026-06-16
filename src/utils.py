2e031ef5de628ab2182c39d0914151934e00225b
Udeani4 <udeaniizu04@gmail.com>
2026-06-16 17:24:58 +0100

    Hyper parameter successful

--- src/utils.py
+++ src/utils.py
@@ -5,6 +5,7 @@ import numpy as np
 import pandas as pd
 import dill ## This is also used to store an object as a pickle file
 from sklearn.metrics import r2_score
+from sklearn.model_selection import GridSearchCV
 
 from src.exception import CustomException
 
@@ -21,15 +22,25 @@ def save_object(file_path,obj):
     except Exception as e:
         raise CustomException(e,sys)
 
-def evaluate_models(X_train,y_train,X_test,y_test,models):
+def evaluate_models(X_train,y_train,X_test,y_test,models,params):
     try:
         training_report = {}
         test_report={}
 
         for i in range(len(list(models))):
             model=list(models.values())[i]
+            param=params[list(models.keys())[i]]
 
-            model.fit(X_train,y_train) #Train model
+            grid=GridSearchCV(model,param_grid=param,cv=3,error_score='raise')
+            grid.fit(X_train,y_train)
+
+            model.set_params(**grid.best_params_) ## this will unpack the best_params_ dictionary into keyword arguements. It will now set that param as the parameters in the model
+            model.fit(X_train,y_train) ## This s now the model with the best parameters we are training
+
+            ## Another more direct way once the grid search is done fitting..
+            # best_model = grid.best_estimator_
+            # y_pred = best_model.predict(X_test)
+            ## The above is a more generic approach
 
             y_train_pred=model.predict(X_train)
             y_test_pred=model.predict(X_test)
@@ -38,7 +49,7 @@ def evaluate_models(X_train,y_train,X_test,y_test,models):
             test_model_score=r2_score(y_test,y_test_pred)
 
             training_report[list(models.keys())[i]]=train_model_score
-            
+
             test_report[list(models.keys())[i]]=test_model_score
 
         return training_report, test_report
